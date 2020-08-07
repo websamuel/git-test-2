0a622f6d1c98a317f8fd546547986d9c6fe737fb
websamuel <rickson239@gmail.com>
2020-08-07 13:32:30 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello rickson</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos hic aliquam harum, mollitia error natus eos nostrum optio velit fugiat adipisci laboriosam, ipsa sapiente officiis distinctio corrupti tenetur, totam numquam!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
